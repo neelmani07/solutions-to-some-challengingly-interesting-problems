641275c3e43b4e47048654b4d674258d99679856
neelmani07 <manineel25@gmail.com>
2020-07-11 16:31:07 +0530

    11/7

---
+++ PruneTree.java
@@ -0,0 +1,50 @@
+/*
+We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+
+Example 2:
+Input: [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+
+Example 3:
+Input: [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root==null)
+			return null;
+		if(root.val==0 && root.left==null && root.right==null)
+		{ 
+			return null;
+		}
+		root.left=pruneTree(root.left);
+		root.right=pruneTree(root.right);
+        if(root.left==null && root.right==null && root.val==0)
+            return null;
+		return root;
+        
+    }
+}
