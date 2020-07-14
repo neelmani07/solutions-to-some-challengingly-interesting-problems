3d81c5149d5f3e0cc223a984ca0af90240a5917e
neelmani07 <manineel25@gmail.com>
2020-07-14 11:25:56 +0530

    13/7

---
+++ ReachANoInMinSteps.java
@@ -0,0 +1,43 @@
+/*
+ *  You are standing at position 0 on an infinite number line. There is a goal at position target.
+
+On each move, you can either go left or right. During the n-th move (starting from 1), you take n steps.
+
+Return the minimum number of steps required to reach the destination.
+
+Example 1:
+
+Input: target = 3
+Output: 2
+Explanation:
+On the first move we step from 0 to 1.
+On the second step we step from 1 to 3.
+
+Example 2:
+
+Input: target = 2
+Output: 3
+Explanation:
+On the first move we step from 0 to 1.
+On the second move we step  from 1 to -1.
+On the third move we step from -1 to 2.
+
+credits:-https://leetcode.com/articles/reach-a-number/#
+
+ */
+package leetcode;
+
+public class ReachANoInMinSteps {
+
+    public static int reachNumber(int target) {
+        target = Math.abs(target);
+        int k = 0;
+        while (target > 0)
+            target -= ++k;
+        return target % 2 == 0 ? k : k + 1 + k%2;
+    }
+    public static void main(String a[])
+    {
+    	System.out.print(reachNumber(5));
+    }
+}
